068c4c57e6cf25c2a3b23bfee91557b05d46804b
EdithKing <1841308019@qq.com>
2020-01-30 14:51:37 +0800

    bio缺点以及优化

--- java/io/progress/src/main/java/com/study/bio/TimeServer.java
+++ java/io/progress/src/main/java/com/study/bio/TimeServer.java
@@ -18,9 +18,17 @@ public class TimeServer {
 			server = new ServerSocket(port);
 			System.out.println("The time server is start in port : " + port);
 			Socket socket = null;
+			TimeServerHandlerExecutePool executePool = new TimeServerHandlerExecutePool(50, 10000);
 			while (true) {
 				socket = server.accept();
-				new Thread(new TimeServerHandler(socket)).start();
+				/**
+				 * 同步阻塞io，一个客户端请求一个线程
+				 */
+//				new Thread(new TimeServerHandler(socket)).start();
+				/**
+				 * 采用线程池，避免线程太多
+				 */
+				executePool.execute(new TimeServerHandler(socket));
 			}
 
 		} finally {
