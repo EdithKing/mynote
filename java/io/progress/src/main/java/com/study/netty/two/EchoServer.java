7a7b79020048f8605a9218eb5db45b0df77f06be
EdithKing <1841308019@qq.com>
2020-01-31 22:32:12 +0800

    新增FixedLengthFrameDecoder解决tcp粘包拆包问题

---
+++ java/io/progress/src/main/java/com/study/netty/two/EchoServer.java
@@ -0,0 +1,50 @@
+package com.study.netty.two;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class EchoServer {
+
+	public void bind(int port) throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 100)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+							ch.pipeline().addLast(new StringDecoder());
+							ch.pipeline().addLast(new EchoServerHandler());
+						}
+					});
+			ChannelFuture channelFuture = b.bind(port).sync();
+			channelFuture.channel().close().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workGroup.shutdownGracefully();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		int port = 3333;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+			}
+		}
+		new EchoServer().bind(port);
+	}
+
+}
---
+++ java/io/progress/src/main/java/com/study/netty/two/EchoServerHandler.java
@@ -0,0 +1,17 @@
+package com.study.netty.two;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class EchoServerHandler extends ChannelHandlerAdapter {
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println("Reveive client : [ " + msg + " ]");
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
